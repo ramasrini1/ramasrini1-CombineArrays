e8c3543b5b5c892a0e9a499e11eef8140b558637
Rama Srinivas <ramasrini1@gmail.com>
2023-03-07 07:05:14 +0000

    combing arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -9,6 +9,15 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+        int size = arr1.length + arr2.length;
+        int size1 = arr1.length;
+        int [] newArr = new int[size];
+        for( int i=0; i<arr1.length; i++ ){
+            newArr[i] = arr1[i];
+        }
+        for (int i=0; i<arr2.length; i++ ){
+            newArr[i + size1] = arr2[i];
+        }
+        return newArr;
     }
 }
